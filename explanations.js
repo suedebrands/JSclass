7a0958d22d897bc3df9c1d41a8f20a6a8a59ae94
suedebrands <spolarmatt@Matts-MacBook-Pro-2.local>
2017-02-27 20:31:21 -0500

    stuff

---
+++ explanations.js
@@ -0,0 +1,16 @@
+// Explanations 
+var favColor = 'red';
+
+var myFavColors = ['blue', 'red', 'green'];
+
+var numOfFavColors = 3; 
+
+var hasGot = true;
+
+var richObject = {
+	firstName: 'Rich',
+	lastName: 'Armstrong', 
+	yFavColors: ['blue', 'red', 'green'], 
+	yearsAlive: 103, j
+	isMale: true 
+};
